9194ae9eb57975cb97c3d1c570d79d804270517c
Pavel Trofimov <pavel.trofimov.tot@mail.ru>
2023-03-24 23:40:21 +0700

    11

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import routesConfig from 'routes/routesConfig';
 import Header from 'components/Header';
+import { REPO_NAME } from 'constants/repo';
 
 import styles from './App.module.css';
 
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
       <div className={styles.wrapper}>
         <Header />
 
